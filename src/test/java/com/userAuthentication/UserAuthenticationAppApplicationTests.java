949aa7582503cfd9af3494ce33d77cb726aff1a6
Elisee Augustin <elisee@Elisees-MBP.home>
2020-01-25 22:48:21 -0500

    first commit, created project

---
+++ src/test/java/com/userAuthentication/UserAuthenticationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userAuthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
