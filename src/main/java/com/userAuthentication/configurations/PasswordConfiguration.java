be70d1df2bcd75eb28bab682da0b8ab0a6fbfe43
Elisee Augustin <elisee@Elisees-MBP.home>
2020-01-26 22:02:02 -0500

    created a configuration class for password hashing+salt using BCryptPasswordEncoder

---
+++ src/main/java/com/userAuthentication/configurations/PasswordConfiguration.java
@@ -0,0 +1,21 @@
+package com.userAuthentication.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfiguration {
+
+	@Bean
+	
+	
+	public PasswordEncoder passwordEncoder() {
+		//BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		//return encoder;
+		
+		return new BCryptPasswordEncoder();
+	}
+
+}
